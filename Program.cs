b4795018261218191807a8cc57f4b1252c2e8f5d
whiteyeOwO <betty06231227@gmail.com>
2025-04-25 21:25:39 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace HW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0;
+            int max = 99;
+            Random rn = new Random();
+            int bingo = rn.Next(min , max + 1);
+
+            while (true)
+            {
+                Console.WriteLine("({0}, {1})?",min,max);
+                int ans = int.Parse(Console.ReadLine());
+                if (ans > max || ans < min)
+                {
+                    Console.WriteLine("Out of range. Try again?");
+                    continue;
+                }
+                else if (ans < bingo) 
+                { 
+                    min = ans + 1;
+                    if (max - min <= 1)
+                    {
+                       Console.WriteLine("GG.");
+                        break;
+                    }
+                    else
+                    {
+                       continue;
+                    }
+                }
+                else if (ans > bingo)
+                {
+                    max = ans - 1;
+                    if (max - min <= 1)
+                    {
+                        Console.WriteLine("GG.");
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (ans == bingo)
+                {
+                    Console.WriteLine("Bingo!!");
+                    break;
+                }
+            }
+        }
+    }
+}
